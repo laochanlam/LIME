4802a7036a7d2260962e85e2e986b9a3102a3e34
laochanlam <laochanlam@gmail.com>
2017-05-06 02:20:45 +0800

    Event listener add

--- LoginWindow.java
+++ LoginWindow.java
@@ -5,33 +5,42 @@ public class LoginWindow {
     public LoginWindow() {
         // Window
         loginWindow = new JFrame();
-        loginWindow.setSize(400, 100);
+        loginWindow.setSize(400, 120);
         loginWindow.setLocation(300, 200);
-        
 
         // Text Area
-        JTextField userName = new JTextField("Username", 30);
-        JTextField password = new JTextField("Password", 30);
+        JTextField userName = new JTextField(30);
+        JTextField password = new JTextField(30);
         JLabel userNameLab = new JLabel("用戶名");
         JLabel passwordLab = new JLabel("密碼");
 
-        // Layout
-        JPanel panel = new JPanel(new GridLayout());
-        GridBagConstraints parameter = new GridBagConstraints();
-        parameter.gridx = 100;
-        parameter.gridy = 0;
-        parameter.gridwidth = 100;
-        parameter.gridheight = 100;
+        // Button
+        JButton submitButton = new JButton("登入");
+        submitButton.addActionListener(new LoginEventListener());
+        JButton registerButton = new JButton("注冊");
+        registerButton.addActionListener(new RegisterEventListener());
+
+        // TODO: MAKE EVERYTHING INTO ONE PANEL
+        // JPanel panel = new JPanel(new GridLayout());
+        // GridBagConstraints parameter = new GridBagConstraints();
+        // parameter.gridx = 100;
+        // parameter.gridy = 0;
+        // parameter.gridwidth = 100;
+        // parameter.gridheight = 100;                             
 
         
-        loginWindow.setLayout(new GridLayout(2, 2));
-        loginWindow.add(userNameLab, parameter);
+        // Layout
+        loginWindow.setLayout(new GridLayout(3, 2));
+        loginWindow.add(userNameLab);
         loginWindow.add(userName);
         loginWindow.add(passwordLab);
         loginWindow.add(password);
+        loginWindow.add(submitButton);
+        loginWindow.add(registerButton);
     }
 
     public void show() {
         loginWindow.setVisible(true);
     }
 }
+
---
+++ RegisterEventListener.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RegisterEventListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        System.out.print("Register");
+    }
+
+}
