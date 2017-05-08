a604616d21045544a2ca03db8e278d320d11ef49
laochanlam <laochanlam@gmail.com>
2017-05-09 01:16:11 +0800

    Login function completed

--- LoginWindow.java
+++ LoginWindow.java
@@ -13,29 +13,18 @@ public class LoginWindow extends JFrame implements ActionListener {
         setSize(400, 120);
         setLocation(300, 200);
 
-        // Text Area
+        // Text Field
         userNameTextField = new JTextField(30);
         passwordTextField = new JTextField(30);
         JLabel userNameLabel = new JLabel("用戶名");
         JLabel passwordLabel = new JLabel("密碼");
         
-
-        // System.out.print(userName + password);
         // Button
         loginButton = new JButton("登入");
         loginButton.addActionListener(this);
         registerButton = new JButton("注冊");
         registerButton.addActionListener(this);
 
-        // TODO: MAKE EVERYTHING INTO ONE PANEL
-        // JPanel panel = new JPanel(new GridLayout());
-        // GridBagConstraints parameter = new GridBagConstraints();
-        // parameter.gridx = 100;
-        // parameter.gridy = 0;
-        // parameter.gridwidth = 100;
-        // parameter.gridheight = 100;                             
-
-        
         // Layout
         setLayout(new GridLayout(3, 2));
         add(userNameLabel);
@@ -44,31 +33,59 @@ public class LoginWindow extends JFrame implements ActionListener {
         add(passwordTextField);
         add(loginButton);
         add(registerButton);
+    
+        // Connect with DB
+
+        // TODO: MAKE EVERYTHING INTO ONE PANEL
+        // JPanel panel = new JPanel(new GridLayout());
+        // GridBagConstraints parameter = new GridBagConstraints();
+        // parameter.gridx = 100;
+        // parameter.gridy = 0;
+        // parameter.gridwidth = 100;
+        // parameter.gridheight = 100;                             
     }
 
     public void actionPerformed(ActionEvent e) {
+        String userName = userNameTextField.getText();
+        String password = passwordTextField.getText();
+        DatabaseInfo databaseInfo = new DatabaseInfo();
+        Connection connection = null;
+        Statement statement = null;
+        String JDBC_DRIVER = databaseInfo.getDrive();
+        String DB_URL = databaseInfo.getUrl();
+        String USER = databaseInfo.getUser();
+        String PASS = databaseInfo.getPassword();
+        
+        
         if (e.getSource() == loginButton) {
-            System.out.println("[Login]");
+            try{
+                System.out.println("[Login]");
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(DB_URL, USER, PASS);
+                System.out.println("Connected database successfully...");
+
+                statement = connection.createStatement();
+
+                String sql = "SELECT * FROM user WHERE name = '" + userName + "' AND password = '" + password + "'";
+                ResultSet rs = statement.executeQuery(sql);
+                if (!rs.next())
+                    System.out.println("[Query] Fail!");
+                else 
+                    System.out.println("[Query] Success!");
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } catch (Exception ee) {
+                ee.printStackTrace();
+            } 
+
         }
 
         if (e.getSource() == registerButton) {
             System.out.println("[Register]");
-            DatabaseInfo databaseInfo = new DatabaseInfo();
-
-            Connection connection = null;
-            Statement statement = null;
-            String JDBC_DRIVER = databaseInfo.getDrive();
-            String DB_URL = databaseInfo.getUrl();
-            String USER = databaseInfo.getUser();
-            String PASS = databaseInfo.getPassword();
-
-            String userName = userNameTextField.getText();
-            String password = passwordTextField.getText();
 
             // Work with Database
             try{
                 Class.forName("com.mysql.jdbc.Driver");
-
                 connection = DriverManager.getConnection(DB_URL, USER, PASS);
                 System.out.println("Connected database successfully...");
 
